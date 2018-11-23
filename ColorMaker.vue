e757e200313667d3c0a6bdc4410b4aea12562b5f
LorisC <loris@ciprin.fr>
2018-11-23 10:12:40 +0100

    first commit

---
+++ ColorMaker.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="colorItem" :style="{background: color,  width: width}">
+        <div :style="{visibility: 'hidden'}">
+            color
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ColorMaker",
+        props: {
+            color: {
+                type: String,
+                default: function () {
+                    return "";
+                }
+            },
+            width: {
+                type: String,
+                default: function () {
+                    return "10px";
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
